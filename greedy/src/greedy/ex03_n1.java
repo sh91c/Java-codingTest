b3415794bf639b71f94115a2b7698b0d45df9548
Choi Sang Hoon <sh91cxp@gmail.com>
2020-10-28 11:54:57 +0900

    그리디 : N을 1로 만들기
    N과 K를 입력받아 두 가지 연산을 통해 N을 1로 만들면서 최소한의 연산 횟수를 구하기

---
+++ greedy/src/greedy/ex03_n1.java
@@ -0,0 +1,34 @@
+package greedy;
+
+import java.util.Scanner;
+
+// 어떠한 수 N이 1이 될 때까지
+public class ex03_n1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// N, K 공백을 기준으로 구분해 입력받기
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int count = 0;
+
+		while (true) {
+			// N이 K로 나누어 떨어지는 수가 될 때까지 빼기
+			int target = (n / k) * k; // 반복문을 쓰지않고 K가 나눌수 있는 N에 가까운 수를 찾음
+			count += (n - target); // N과 찾은 수를 뺀 값이, -1 계산 횟수를 구한 값과 같음 -> count에 담음
+			n = target; // K에 나눠지는 값을 N에 대입
+
+			// K로 나눈 값을 다시 N에 넣고 count 1증가
+			n /= k;
+			count += 1;
+
+			// N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+			if (n < k)
+				break;
+		}
+		// 마지막으로 남은 수(K보다 작아진 N)에 대해 1씩 빼기
+		count += (n - 1);
+		System.out.println(count);
+	}
+}
---
+++ greedy/src/greedy/ex03_n_easy.java
@@ -0,0 +1,32 @@
+package greedy;
+
+import java.util.Scanner;
+
+// 어떠한 수 N이 1이 될 때까지
+public class ex03_n_easy {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// N, K 공백을 기준으로 구분해 입력받기
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int count = 0;
+
+		while( n >= k ) {
+			if( n%k != 0) {
+				n--;
+				count++;
+			} else {
+				n /= k;
+				count++;
+			}
+		}
+		
+		while (n > 1) {
+			n--;
+			count++;
+		}
+		System.out.println(count);
+	}
+}
