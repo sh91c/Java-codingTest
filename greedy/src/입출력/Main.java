f77dc7871824273394c08faf09c1e0d49591196b
Choi Sang Hoon <sh91cxp@gmail.com>
2020-10-29 20:34:34 +0900

    Update Main.java

--- greedy/src/입출력/Main.java
+++ greedy/src/입출력/Main.java
@@ -1,18 +1,27 @@
 package 입출력;
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-
-		while (sc.hasNextInt()) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-
-			System.out.println(a+b);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j <= n-1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i*2-1; j++) {
+				if(j % n*i == 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
 		}
-		System.out.println(sc.hasNextInt());
 	}
 }
